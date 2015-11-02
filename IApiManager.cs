cf9e0188f0ad195b5df5dd7659089334c2590088
Theofanis Tokas <tokastheofanis@gmail.com>
2015-11-02 14:49:52 +0200

    Add new interface IApiManager.
    This interface now describes ApiManager's functionality.

---
+++ IApiManager.cs
@@ -0,0 +1,18 @@
+﻿using Moosend.API.Client.Wrappers;
+
+namespace Moosend.API.Client
+{
+    public interface IApiManager
+    {
+        string ApiKey { get; set; }
+        CampaignsWrapper Campaigns { get; }
+        MailingListsWrapper MailingLists { get; }
+        SegmentsWrapper Segments { get; }
+        SubscribersWrapper Subscribers { get; }
+
+        void MakeRequest(HttpMethod method, string path);
+        void MakeRequest(HttpMethod method, string path, object parameters);
+        T MakeRequest<T>(HttpMethod method, string path);
+        T MakeRequest<T>(HttpMethod method, string path, object parameters);
+    }
+}
