6542046a1fda38af67ee0df66988ad799651eb3d
Giannis Tourkogiorgis <john@moosend.com>
2016-01-19 12:42:37 +0200

    Add *.Client project

---
+++ v3/Moosend.Api.Client/HttpRequestMessageFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Moosend.Api.Client
+{
+    public class HttpRequestMessageFactory
+    {
+        public HttpRequestMessage Create(HttpMethod method, Uri endpoint, string path, string apiKey, IDictionary<string, string> queryParams = null)
+        {
+            if (method == null) throw new ArgumentNullException("method");
+            if (endpoint == null) throw new ArgumentNullException("endpoint");
+            if (path == null) throw new ArgumentNullException("path");
+            if (apiKey == null) throw new ArgumentNullException("apiKey");
+
+            var sb = new StringBuilder(string.Format("{0}{1}?apiKey={2}",
+                endpoint,
+                path,
+                apiKey));
+
+            if (queryParams != null)
+            {
+                foreach (var queryParam in queryParams)
+                {
+                    sb.Append(string.Format("&{0}={1}", queryParam.Key, queryParam.Value));
+                }
+            }
+
+            var uri = new Uri(endpoint, sb.ToString());
+
+            return new HttpRequestMessage(method, uri);
+        }
+    }
+}
