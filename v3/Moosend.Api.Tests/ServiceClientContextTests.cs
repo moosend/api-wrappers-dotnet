2ca8d6c857fb2ddbb0a6c533f41e324b31028db1
Giannis Tourkogiorgis <john@moosend.com>
2016-01-19 12:43:27 +0200

    Add *.Tests project

---
+++ v3/Moosend.Api.Tests/ServiceClientContextTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Moosend.Api.Client;
+using NUnit.Framework;
+
+namespace Moosend.Api.Tests
+{
+    [TestFixture]
+    public class ServiceClientContextTests
+    {
+        [Test]
+        public void Given_Null_Endpoint_When_Creating_ServiceClientContext_Then_It_Throws()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => new ServiceClientContext(null, "api_key"));
+        }
+
+        [Test]
+        public void Given_Null_ApiKey_When_Creating_ServiceClientContext_Then_It_Throws()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => new ServiceClientContext(new Uri("http://endpoint"), null));
+        }
+
+        [Test]
+        public void Given_Valid_Arguments_When_Creating_ServiceClientContext_Then_It_Can_Create_Instance()
+        {
+            var apiKey = "api_key";
+            var endpoint = new Uri("http://endpoint");
+            var context = new ServiceClientContext(endpoint, apiKey);
+
+            Assert.That(context, Is.InstanceOf<ServiceClientContext>());
+            Assert.That(context.Endpoint, Is.EqualTo(endpoint));
+            Assert.That(context.ApiKey, Is.EqualTo(apiKey));
+        }
+    }
+}
