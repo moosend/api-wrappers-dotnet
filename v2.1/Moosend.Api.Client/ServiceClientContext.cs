90fc7733fe902ee2693191985f2ec146db4f5a8a
Giannis Tourkogiorgis <john@moosend.com>
2016-01-20 22:27:44 +0200

    Update `ServiceClientContext`, change assembly version in `AssemblyInfo` and update packages.config file

---
+++ v2.1/Moosend.Api.Client/ServiceClientContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Moosend.Api.Client
+{
+    public class ServiceClientContext
+    {
+        public ServiceClientContext(Uri endpoint, string apiKey)
+        {
+            if (endpoint == null) throw new ArgumentNullException("endpoint");
+            if (apiKey == null) throw new ArgumentNullException("apiKey");
+
+            Endpoint = endpoint;
+            ApiKey = apiKey;
+            // TODO set right timeout
+            Timeout = TimeSpan.FromSeconds(10);
+        }
+
+        public Uri Endpoint { get; private set; }
+        public TimeSpan Timeout { get; set; }
+        public HttpMessageHandler Handler { get; set; }
+        public string ApiKey { get; private set; }
+    }
+}
