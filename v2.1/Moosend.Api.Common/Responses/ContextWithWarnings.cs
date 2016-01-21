8c7868258eba903ec7e0778370a225bcae251836
Giannis Tourkogiorgis <john@moosend.com>
2016-01-21 23:07:33 +0200

    Add models `ContextWithWarning` and `MessageWarning`

---
+++ v2.1/Moosend.Api.Common/Responses/ContextWithWarnings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Moosend.Api.Common.Responses
+{
+    public class ContextWithWarnings
+    {
+        public Guid Id { get; set; }
+        public IList<MessageWarning> Messages { get; set; }
+    }
+}
---
+++ v2.1/Moosend.Api.Common/Responses/MessageWarning.cs
@@ -0,0 +1,8 @@
+﻿namespace Moosend.Api.Common.Responses
+{
+    public class MessageWarning
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
