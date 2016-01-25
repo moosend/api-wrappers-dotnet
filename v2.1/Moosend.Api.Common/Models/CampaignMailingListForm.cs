6fa33f5895d52010b109242d4643b7bf5d6c24b9
Giannis Tourkogiorgis <john@moosend.com>
2016-01-25 12:16:14 +0200

    Add model `CampaignMailingListForm`

---
+++ v2.1/Moosend.Api.Common/Models/CampaignMailingListForm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Moosend.Api.Common.Models
+{
+    public class CampaignMailingListForm
+    {
+        public Guid MailingListId { get; set; }
+        public int SegmentId { get; set; }
+
+    }
+}
