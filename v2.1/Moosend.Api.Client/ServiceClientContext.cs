75e1407d624f1a813e69b7b9a0e598a623602ee3
Giannis Tourkogiorgis <john@moosend.com>
2016-01-25 17:55:52 +0200

    Remove Apikey from `ServiceClientContext`
    - The api key is now passed to `MoosendApiClient` constructor

--- v2.1/Moosend.Api.Client/ServiceClientContext.cs
+++ v2.1/Moosend.Api.Client/ServiceClientContext.cs
@@ -5,14 +5,11 @@ namespace Moosend.Api.Client
 {
     public class ServiceClientContext
     {
-        // TODO maybe set default endpoint api v3 or make it a const in MoosendApiClient
-        public ServiceClientContext(Uri endpoint, string apiKey)
+        public ServiceClientContext(Uri endpoint)
         {
             if (endpoint == null) throw new ArgumentNullException("endpoint");
-            if (apiKey == null) throw new ArgumentNullException("apiKey");
 
             Endpoint = endpoint;
-            ApiKey = apiKey;
             // TODO set right timeout
             Timeout = TimeSpan.FromSeconds(10);
         }
@@ -20,6 +17,5 @@ namespace Moosend.Api.Client
         public Uri Endpoint { get; private set; }
         public TimeSpan Timeout { get; set; }
         public HttpMessageHandler Handler { get; set; }
-        public string ApiKey { get; private set; }
     }
 }
