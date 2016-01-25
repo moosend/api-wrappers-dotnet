8133c979f6dc6c303d77f04bb922b6abe9902cc7
Giannis Tourkogiorgis <john@moosend.com>
2016-01-25 12:11:59 +0200

    Update API error codes

--- v2.1/Moosend.Api.Common/Responses/ApiCodes.cs
+++ v2.1/Moosend.Api.Common/Responses/ApiCodes.cs
@@ -4,8 +4,13 @@
     {
         public const int SUCCESS = 0;
         public const int API_KEY_NOT_VALID = 104;
+        public const int LIST_NOT_FOUND = 200;
+        public const int SEGMENT_NOT_FOUND = 202;
         public const int VALIDATION_ERROR = 501;
         public const int CAMPAIGN_NOT_FOUND = 400;
         public const int CAMPAIGN_STATISTICS_NOT_FOUND = 401;
+        public const int SENDER_NOT_FOUND = 402;
+        public const int CANNOT_UPDATE_NON_DRAFT_CAMPAIGN = 403;
+
     }
 }
