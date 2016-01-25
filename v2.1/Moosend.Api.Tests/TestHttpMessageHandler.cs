62400a8ccde4e45282d08d15ed81ecdf873f0f60
Giannis Tourkogiorgis <john@moosend.com>
2016-01-25 17:58:18 +0200

    Add `TestHttpMessageHandler` for testing `MoosendApiClient`

---
+++ v2.1/Moosend.Api.Tests/TestHttpMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Moosend.Api.Tests
+{
+    public class TestHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly HttpStatusCode _code;
+
+        public TestHttpMessageHandler(HttpStatusCode code = HttpStatusCode.NoContent)
+        {
+            _code = code;
+            Payloads = new List<string>();
+            Requests = new List<HttpRequestMessage>();
+        }
+
+        public List<HttpRequestMessage> Requests { get; }
+
+        public List<string> Payloads { get; }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Requests.Add(request);
+            if (request.Content != null) Payloads.Add(request.Content.ReadAsStringAsync().Result);
+
+            return Task.FromResult(new HttpResponseMessage(_code) {Content = new StringContent("{}")});
+        }
+    }
+}
