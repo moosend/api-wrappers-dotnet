e069bbd2e719e867aa522fdad249161441b29ade
Giannis Tourkogiorgis <john@moosend.com>
2016-01-27 14:11:30 +0200

    Modify `TestHttpMessageHandler` to be able to return payload in demand

--- v2.1/Moosend.Api.Tests/TestHttpMessageHandler.cs
+++ v2.1/Moosend.Api.Tests/TestHttpMessageHandler.cs
@@ -3,15 +3,18 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Moosend.Api.Tests
 {
     public class TestHttpMessageHandler : HttpMessageHandler
     {
         private readonly HttpStatusCode _code;
+        private readonly object _responseContent;
 
-        public TestHttpMessageHandler(HttpStatusCode code = HttpStatusCode.NoContent)
+        public TestHttpMessageHandler(HttpStatusCode code = HttpStatusCode.NoContent, object responseContent = null)
         {
+            _responseContent = responseContent;
             _code = code;
             Payloads = new List<string>();
             Requests = new List<HttpRequestMessage>();
@@ -21,12 +24,20 @@ namespace Moosend.Api.Tests
 
         public List<string> Payloads { get; }
 
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
         {
             Requests.Add(request);
             if (request.Content != null) Payloads.Add(request.Content.ReadAsStringAsync().Result);
 
-            return Task.FromResult(new HttpResponseMessage(_code) {Content = new StringContent("{}")});
+            return
+                Task.FromResult(new HttpResponseMessage(_code)
+                {
+                    Content =
+                        new StringContent(_responseContent == null
+                            ? "{}"
+                            : JsonConvert.SerializeObject(_responseContent))
+                });
         }
     }
 }
