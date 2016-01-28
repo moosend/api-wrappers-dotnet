96f60f9f240875caad89d3dcf9e1bb3367ec1a36
Giannis Tourkogiorgis <john@moosend.com>
2016-01-28 17:47:55 +0200

    Modify some models to fit new API version scheme

--- v2.1/Moosend.Api.Common/Models/Segment.cs
+++ v2.1/Moosend.Api.Common/Models/Segment.cs
@@ -15,76 +15,35 @@ namespace Moosend.Api.Common.Models
             mailingListId.CheckNotNull("mailingListID");
 
             Criteria = new List<SegmentCriteria>();
-            this.MailingListId = mailingListId;
+            MailingListId = mailingListId;
         }
 
         public Segment(MailingList mailingList, string name, SegmentMatchType matchType = SegmentMatchType.All)
         {
             Criteria = new List<SegmentCriteria>();
-            this.MailingListId = mailingList.Id;
-            this.Name = name;
-            this.MatchType = matchType;
+            MailingListId = mailingList.Id;
+            Name = name;
+            MatchType = matchType;
         }
 
-        public int Id
-        {
-            get;
-            internal set;
-        }
+        public int Id { get; set; }
 
-        public string Name
-        {
-            get;
-            set;
-        }
+        public string Name { get; set; }
 
-        public SegmentMatchType MatchType
-        {
-            get;
-            set;
-        }
+        public SegmentMatchType MatchType { get; set; }
 
-        public IList<SegmentCriteria> Criteria
-        {
-            get;
-            internal set;
-        }
+        public IList<SegmentCriteria> Criteria { get; set; }
 
-        public long MemberCount
-        {
-            get;
-            internal set;
-        }
+        public long MemberCount { get; set; }
 
-        public string CreatedBy
-        {
-            get;
-            internal set;
-        }
+        public string CreatedBy { get; set; }
 
-        public DateTime CreatedOn
-        {
-            get;
-            internal set;
-        }
+        public DateTime CreatedOn { get; set; }
 
-        public string UpdatedBy
-        {
-            get;
-            internal set;
-        }
+        public string UpdatedBy { get; set; }
 
-        public DateTime UpdatedOn
-        {
-            get;
-            internal set;
-        }
+        public DateTime UpdatedOn { get; set; }
 
-        public Guid MailingListId
-        {
-            get;
-            internal set;
-        }
-
+        public Guid MailingListId { get; set; }
     }
 }
--- v2.1/Moosend.Api.Common/Models/SegmentCriteria.cs
+++ v2.1/Moosend.Api.Common/Models/SegmentCriteria.cs
@@ -4,53 +4,20 @@ namespace Moosend.Api.Common.Models
 {
     public class SegmentCriteria
     {
-        public int Id
-        {
-            get;
-            internal set;
-        }
+        public int Id { get; set; }
 
-        public int SegmentId
-        {
-            get;
-            internal set;
-        }
+        public int SegmentId { get; set; }
 
-        public SegmentCriteriaField Field
-        {
-            get;
-            set;
-        }
+        public SegmentCriteriaField Field { get; set; }
 
-        public Guid? CustomFieldId
-        {
-            get;
-            set;
-        }
-        
-        public SegmentCriteriaComparer Comparer
-        {
-            get;
-            set;
-        }
+        public Guid? CustomFieldId { get; set; }
 
-        public string Value
-        {
-            get;
-            set;
-        }
+        public SegmentCriteriaComparer Comparer { get; set; }
 
-        public DateTime? DateFrom
-        {
-            get;
-            set;
-        }
+        public string Value { get; set; }
 
-        public DateTime? DateTo
-        {
-            get;
-            set;
-        }
+        public DateTime? DateFrom { get; set; }
 
+        public DateTime? DateTo { get; set; }
     }
 }
