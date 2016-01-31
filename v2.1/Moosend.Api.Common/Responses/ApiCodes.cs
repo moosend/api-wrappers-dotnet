ad67c2621732ba8ec14a44abaf2cc07cfd5bd034
Giannis Tourkogiorgis <john@moosend.com>
2016-01-31 22:15:13 +0200

    Update ApiCodes.cs

--- v2.1/Moosend.Api.Common/Responses/ApiCodes.cs
+++ v2.1/Moosend.Api.Common/Responses/ApiCodes.cs
@@ -6,6 +6,8 @@
         public const int API_KEY_NOT_VALID = 104;
         public const int LIST_NOT_FOUND = 200;
         public const int SEGMENT_NOT_FOUND = 202;
+        public const int INVALID_FIELD_COMPARER_COMBINATION = 205;
+        public const int DATE_RANGE_NOT_SUPPORTED = 206;
         public const int MEMBER_NOT_FOUND = 300;
         public const int VALIDATION_ERROR = 501;
         public const int CAMPAIGN_NOT_FOUND = 400;
