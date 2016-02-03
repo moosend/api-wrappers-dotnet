b11e453084cbb54fc7a72181c1e0b1fd9094c2e2
Giannis Tourkogiorgis <john@moosend.com>
2016-02-03 20:22:41 +0200

    Update `ApiCodes.cs`

--- v2.1/Moosend.Api.Common/Responses/ApiCodes.cs
+++ v2.1/Moosend.Api.Common/Responses/ApiCodes.cs
@@ -10,11 +10,13 @@
         public const int INVALID_FIELD_COMPARER_COMBINATION = 205;
         public const int DATE_RANGE_NOT_SUPPORTED = 206;
         public const int MEMBER_NOT_FOUND = 300;
-        public const int VALIDATION_ERROR = 501;
         public const int CAMPAIGN_NOT_FOUND = 400;
         public const int CAMPAIGN_STATISTICS_NOT_FOUND = 401;
         public const int SENDER_NOT_FOUND = 402;
         public const int CANNOT_UPDATE_NON_DRAFT_CAMPAIGN = 403;
-
+        public const int CAMPAIGN_IS_ARCHIVED = 405;
+        public const int AB_TEST_CAMPAIGN_NOT_FOUND = 408;
+        public const int CANNOT_MODIFY_SCHEDULE_SETTINGS = 410;
+        public const int VALIDATION_ERROR = 501;
     }
 }
